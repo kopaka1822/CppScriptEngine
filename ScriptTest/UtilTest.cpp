b317d427bf94d3b970eadffba4e8ce7ba041b42d
Felix Brüll <felixbruell@live.de>
2019-02-11 12:54:47 +0100

    added tests for util::makeFunction

--- ScriptTest/UtilTest.cpp
+++ ScriptTest/UtilTest.cpp
@@ -41,7 +41,151 @@ TEST(TestSuite, MakeArray)
 	EXPECT_TRUE(arr->get(0)->equals(NullObject::get()));
 }
 
-TEST(TestSuite, MakeFunction)
+class UtilTest
 {
+public:
+	void mutator(int a, float b)
+	{
+		EXPECT_EQ(a, 5);
+		EXPECT_EQ(b, 6.0f);
+	}
+	void mutatorPtr(int* a, float b)
+	{
+		EXPECT_NE(a, nullptr);
+		EXPECT_EQ(*a, 3);
+		EXPECT_EQ(b, 2.0f);
+		*a = 5;
+	}
+	void mutatorScript(ScriptPtr<IntObject> intObj, int a)
+	{
+		EXPECT_EQ(intObj->getValue(), 6);
+		EXPECT_EQ(a, 9);
+		intObj->add(1);
+		intObj.reset();
+	}
+	void mutatorConstScriptRef(const ScriptPtr<IntObject>& intObj, int a)
+	{
+		EXPECT_EQ(intObj->getValue(), 6);
+		EXPECT_EQ(a, 10);
+		intObj->add(1);
+	}
+	void mutatorScriptRef(ScriptPtr<IntObject>& intObj, int a)
+	{
+		EXPECT_EQ(intObj->getValue(), 9);
+		EXPECT_EQ(a, 7);
+		intObj->add(1);
+		intObj.reset();
+	}
+	void mutatorConst(int a) const
+	{
+		EXPECT_EQ(a, 12);
+	}
+	void mutatorConstPtr(const int* a)
+	{
+		EXPECT_EQ(*a, 13);
+	}
+	void mutatorRef(int& a)
+	{
+		EXPECT_EQ(a, 7);
+		a = 9;
+	}
+	void mutatorConstRef(const int& a)
+	{
+		EXPECT_EQ(a, 14);
+	}
+};
+
+class UtilTestWrapper : public ValueObject<UtilTest>
+{
+public:
+	std::string toString() const override
+	{
+		return "UtilTestWrapper";
+	}
+	ScriptObjectPtr clone() const override
+	{
+		throw ObjectNotCloneableException("UtilTestDerived");
+	}
+};
+
+class UtilTestDerived : GetValueObject<UtilTestDerived>
+{
+public:
+	std::string toString() const override
+	{
+		return "UtilTestDerived";
+	}
+
+	ScriptObjectPtr clone() const override
+	{
+		throw ObjectNotCloneableException("UtilTestDerived");
+	}
+
+	UtilTestDerived& getValue() override
+	{
+		return *this;
+	}
+};
+
+TEST(TestSuite, MakeFunctionForWrapper)
+{
+	auto intObj = std::dynamic_pointer_cast<IntObject>(Util::toScriptObject(1));
 	
+	auto w = std::make_shared<UtilTestWrapper>();
+	UtilTest& t = w->getValue();
+	auto func = Util::makeFunction(w.get(), &t, &UtilTest::mutator, "");
+	ASSERT_TRUE(func);
+	ASSERT_NO_THROW(func(Util::makeArray(5, 6.0f)));
+	
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorPtr, "");
+	ASSERT_TRUE(func);
+	intObj->getValue() = 3;
+	ASSERT_NO_THROW(func(Util::makeArray(intObj, 2.0f)));
+	EXPECT_EQ(intObj->getValue(), 5);
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorScript, "");
+	ASSERT_TRUE(func);
+	intObj->getValue() = 6;
+	ASSERT_NO_THROW(func(Util::makeArray(intObj, 9)));
+	EXPECT_TRUE(intObj);
+	EXPECT_EQ(intObj->getValue(), 7);
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorConstScriptRef, "");
+	ASSERT_TRUE(func);
+	intObj->getValue() = 6;
+	ASSERT_NO_THROW(func(Util::makeArray(intObj, 10)));
+	EXPECT_EQ(intObj->getValue(), 7);
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorScriptRef, "");
+	ASSERT_TRUE(func);
+	intObj->getValue() = 9;
+	ASSERT_NO_THROW(func(Util::makeArray(intObj, 7)));
+	// This should not reset the pointer!
+	ASSERT_TRUE(intObj);
+	EXPECT_EQ(intObj->getValue(), 10);
+
+	//func = decltype(func)(); // assign empty function
+	//func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorConst, "");
+	//EXPECT_TRUE(func);
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorConstPtr, "");
+	ASSERT_TRUE(func);
+	ASSERT_NO_THROW(func(Util::makeArray(13)));
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorRef, "");
+	ASSERT_TRUE(func);
+	intObj->getValue() = 7;
+	ASSERT_NO_THROW(func(Util::makeArray(intObj)));
+	EXPECT_EQ(intObj->getValue(), 9);
+
+	func = decltype(func)(); // assign empty function
+	func = Util::makeFunction(w.get(), &t, &UtilTest::mutatorConstRef, ""); 
+	ASSERT_TRUE(func);
+	ASSERT_NO_THROW(func(Util::makeArray(14)));
 }
