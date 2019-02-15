ab083f43ee1d8c0bb82b8fdce905ca4061cc3cb2
Felix Brüll <felixbruell@live.de>
2019-02-15 23:42:04 +0100

    added first test for script engine

---
+++ ScriptTest/ScriptEngineTest.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include "script/Tokenizer.h"
+
+#define TestSuite ScriptEngineTest
+using namespace script;
+
+TEST(TestSuite, VariablesTest)
+{
+	ScriptEngine engine;
+	decltype(Tokenizer::getExecutable("")) exec;
+	std::string output;
+
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("a = 5"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "5");
+
+	// primitve types
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("b=a"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "5");
+
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("b.add(8)"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "13");
+
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("a"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "13");
+
+	// floats
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("a = 12.0f"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("a = 12f"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+
+	// string
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("myVar =\"test\""));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "\"test\"");
+
+	// bool
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("6.equals(6)"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "true");
+
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("6.0f.equals(6)"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "false");
+
+	// null
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("nll = null"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "null");
+
+	ASSERT_THROW(exec = Tokenizer::getExecutable("null = null"), ParseError);
+
+	// brackets
+	ASSERT_NO_THROW(exec = Tokenizer::getExecutable("([([3, 5, 8].get((1))), 9].get(0))"));
+	ASSERT_NO_THROW(output = exec->execute(engine)->toString());
+	ASSERT_EQ(output, "5");
+}
