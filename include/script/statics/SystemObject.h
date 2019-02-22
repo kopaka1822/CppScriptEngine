8e5d974e676f21c708a0aabb629b2daffe342ff1
Felix Brüll <felixbruell@live.de>
2019-02-22 22:16:59 +0100

    added IOObject

--- include/script/statics/SystemObject.h
+++ include/script/statics/SystemObject.h
@@ -2,6 +2,7 @@
 #include "../ScriptEngine.h"
 #include "ClockObject.h"
 #include "EngineObject.h"
+#include "IOObject.h"
 
 namespace script
 {
@@ -11,8 +12,10 @@ namespace script
 		SystemObject(ScriptEngine &engine);
 		std::shared_ptr<ClockObject> getClock() const;
 		std::shared_ptr<EngineObject> getEngine() const;
+		std::shared_ptr<IOObject> getIO() const;
 	private:
 		std::shared_ptr<ClockObject> m_clock;
 		std::shared_ptr<EngineObject> m_engine;
+		std::shared_ptr<IOObject> m_io;
 	};
 }
--- src/script/statics/SystemObject.cpp
+++ src/script/statics/SystemObject.cpp
@@ -4,9 +4,11 @@ script::SystemObject::SystemObject(ScriptEngine &engine)
 {
 	m_clock = std::make_shared<ClockObject>();
 	m_engine = std::make_shared<EngineObject>(engine);
+	m_io = std::make_shared<IOObject>();
 
 	addFunction("getClock", Util::makeFunction(this, &SystemObject::getClock, "Clock SystemObject::getClock()"));
 	addFunction("getEngine", Util::makeFunction(this, &SystemObject::getEngine, "Engine SystemObject::getEngine()"));
+	addFunction("getIO", Util::makeFunction(this, &SystemObject::getIO, "IO SystemObject::getIO()"));
 }
 
 std::shared_ptr<script::ClockObject> script::SystemObject::getClock() const
@@ -17,4 +19,9 @@ std::shared_ptr<script::ClockObject> script::SystemObject::getClock() const
 std::shared_ptr<script::EngineObject> script::SystemObject::getEngine() const
 {
 	return m_engine;
+}
+
+std::shared_ptr<script::IOObject> script::SystemObject::getIO() const
+{
+	return m_io;
 }
